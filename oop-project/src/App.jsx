1e54dcbddfb58bd5076376c21b9386f18887909e
pharoah4ee <eldalymostafa068@gmail.com>
2026-04-26 21:17:30 +0300

    clean repo (removed ignored files)

--- oop-project/src/App.jsx
+++ oop-project/src/App.jsx
@@ -1,11 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LogIn } from "lucide-react";
+import Login from "./login";
+import Home from "./Home";
 import Navbar from "./Navbar";
 import "./main.css";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-    </>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ oop-project/src/login.jsx
@@ -0,0 +1,18 @@
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <label htmlFor="ID">ID</label>
+        <input type="text" id="ID" name="ID" />
+        <br />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
